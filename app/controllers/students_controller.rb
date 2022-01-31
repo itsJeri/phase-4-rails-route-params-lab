49c4088b15dbcb2d75a744fbe13adb64c70049f6
Jerry Tong <zihangtong@gmail.com>
2022-01-31 15:54:37 -0500

    commit tree 7e6df0c9b7ec795725fc7225662bc3ee50ce5f86

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if request.query_parameters[:name]
+    students = Student.all.where('first_name or last_name = ?', params[:name].capitalize())
     render json: students
+    else 
+      students = Student.all
+      render json: students
+    end
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
